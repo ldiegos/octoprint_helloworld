ab9f132dc4cd26e5471404f54d1a922f92c44092
ldiegoes <ki@ki.es>
2022-10-30 08:04:34 +0100

    0.1.0.5

--- octoprint_helloworld/__init__.py
+++ octoprint_helloworld/__init__.py
@@ -16,9 +16,50 @@ from octoprint.util import dict_merge
 from octoprint.util.version import is_octoprint_compatible
 
 
-class HelloWorldPlugin(octoprint.plugin.StartupPlugin):
+#class HelloWorldPlugin(octoprint.plugin.StartupPlugin):
+class HelloWorldPlugin(FilamentManagerApi,
+                            octoprint.plugin.StartupPlugin,
+                            octoprint.plugin.ShutdownPlugin,
+                            octoprint.plugin.SettingsPlugin,
+                            octoprint.plugin.AssetPlugin,
+                            octoprint.plugin.TemplatePlugin,
+                            octoprint.plugin.EventHandlerPlugin):
+
     def on_after_startup(self):
         self._logger.info("Hello World!")
+
+    def on_shutdown(self):
+        if self.filamentManager is not None:
+            self.filamentManager.close()
+
+    def _printJobPaused(self):
+        # do nothing
+        pass
+
+    def _printJobResumed(self):
+        # do nothing
+        pass
+
+    # Softwareupdate hook
+
+    def get_update_information(self):
+        return dict(
+            filamentmanager=dict(
+                displayName="Filament Manager",
+                displayVersion=self._plugin_version,
+
+                # version check: github repository
+                type="github_release",
+                user="OllisGit",
+                repo="OctoPrint-FilamentManager",
+                current=self._plugin_version,
+
+                # update method: pip
+                #pip="https://github.com/malnvenshorn/OctoPrint-FilamentManager/archive/{target_version}.zip"
+                pip="https://github.com/OllisGit/OctoPrint-FilamentManager/releases/latest/download/master.zip"
+            )
+        )
+
 
 __plugin_pythoncompat__ = ">=3.7,<4"
 #__plugin_implementation__ = HelloWorldPlugin()
@@ -36,8 +77,8 @@ def __plugin_load__():
     #__plugin_implementation__ = FilamentManagerPlugin()
     __plugin_implementation__ = HelloWorldPlugin()
 
-    #global __plugin_hooks__
-    #__plugin_hooks__ = {
-    #    "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information #,
     #    "octoprint.comm.protocol.gcode.sent": __plugin_implementation__.filament_odometer
-    #}
+    }
